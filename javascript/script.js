8aa9487d4e7089a20d4c53ea91932eafb38603fb
munna <enyisuccesskel@gmail.com>
2023-07-05 21:30:41 +0100

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,343 @@
+// // // document.getElementById("count-el").innerText = 14 + 3 + "5"
+// // // function increase(){
+// // //     console.log("the button was clicked")
+// // // }
+// // // let countEl = document.getElementById("count-el");
+
+// // // let count = 0
+ 
+// // // function increase(){
+// // //    count = count + 1
+// // //    countEl.textContent = count
+ 
+// // // }
+// // // let saveEl = document.getElementById("save-nw")
+
+// // // function savebtn(){
+// // //     countStr = count + " - "
+// // //     saveEl.textContent += countStr
+// // //     countEl.textContent =  0
+// // //     count = 0
+
+// // // }
+
+
+
+// // //  let addressed = "Munna";
+// // // greeting = "hi, my name is";
+// // // myGreeting = greeting +" "+ addressed ;
+// // // console.log(myGreeting)
+// // //  let welcomeEl = document.getElementById("welcome-el");
+
+// // //  myName = "Munnachi Enyi"
+// // //  greeting = "Welcome back"
+// // //  welcomeMessage = greeting +" "+ myName;
+// // //  welcomeEl.innerText = welcomeMessage;
+
+// // //  welcomeEl.innerText += "👋"
+
+// // // firstName = "Munnachi ";
+// // // lastName = "Enyi"
+// // // fullNmae = firstName + lastName
+// // // console.log(fullNmae)
+
+// // // let name = "linda"
+// // // greeting = "hi there, "
+
+// // // function together(){
+// // //     console.log( greeting + name )
+// // // }
+// // // together()
+
+// // // let myPoints = 3
+// // //  function add3points(){
+// // //     myPoints += 3 
+// // //  }
+ 
+// // //  function remove1Point(){
+// // //      myPoints -= 1 
+// // //  }
+// // //  add3points()
+// // //  add3points() 
+// // //  add3points()
+// // //  remove1Point()
+// // //  remove1Point()
+// // //  console.log(myPoints)
+// // // let num1 = 8 
+// // // let num2 = 3
+// // // document.getElementById("num1-el").textContent = 8
+// // // document.getElementById("num2-el").textContent = 3
+// // // let sumEl = document.getElementById("sum-el")
+// // //  function add(){
+// // //     let result = num1 + num2
+// // //  sumEl.textContent = "Sum:" + result
+// // //  }
+// // //  function subtract(){
+// // //     let result = num1 - num2;
+// // //     sumEl.textContent = "Sum: " + result
+// // //     }
+// // //     function divide(){
+// // //         let result = num1 / num2
+// // //         sumEl.textContent = "Sum:" + result
+// // //         }
+// // //         function multiply(){
+// // //             let result = num1 * num2
+// // //             sumEl.textContent = "Sum:" + result
+// // //             }
+// // //  let firstcard = 9 ; 
+// // //  let secondCard = 5;
+// // //  sum = firstcard + secondCard;
+// // //  console.log(sum)
+
+// // // //  let age = 23
+
+// // //  if (age < 21){
+// // //     console.log("You can't get into the club")
+// // //  } else
+// // //  console.log("welcome to our club")
+
+// // //  let age = 100
+// // //  cancollect = false 
+// // //  if (age < 100){
+// // //     console.log("Not Eligible yet")
+// // //  } else if ( age === 100){
+// // //     console.log("here is your birthday card from the king")
+// // //     cancollect = true
+// // //  } else {
+// // //     console.log("You have already gottten one,Not eligible")
+// // //  }
+
+// // //  console.log(cancollect)
+
+// // // array in javascript
+// // //  let skills = ["hey, do you know i have broad knowledge about blockchain and not bitcoin",
+// // //  "i have major storytelling skills that got me a gig with daily.dev",
+// // // ]
+// // // //  console.log(skills[0])
+// // // let experience = [
+// // // "Frontend developer at xeneta", 
+// // // "peopele counter for Norstat",
+// // // "ceo at scrimba"]
+// // // console.log(experience[0])
+// // // console.log(experience[1])
+// // // console.log(experience[2])
+
+// // // let getToKnowMe = [
+// // //    "Munnach Enyi",
+// // //    19,
+// // //    false
+// // // ] 
+// // // console.log(getToKnowMe[0])
+// // // console.log(getToKnowMe[2])
+// // // console.log(getToKnowMe[1])
+
+// // // pushing or adding an array
+
+// // // let messages = [
+// // //    "hey,how is it going?",
+// // //    "I'm great, thank you! how about you?",
+// // //    "All good. been working on my portfolio today",
+// // //   "Same here"
+// // // ]
+// // // newmessage = "Same here"
+
+// // // messages.push("Same here")
+// // // messages.push("Good to know")
+
+// // // messages.pop()
+
+// // // console.log(messages)
+
+// // // how to count in javascript
+
+// // // for (let count = 10; count <= 20; count += 2 ){
+// // //    console.log(count)
+
+// // // }
+  
+
+// // // how to create a loop in javascript using the method of counting in javascript
+// // // "you can use the counting method in js to merge arrays and loops together"
+// // // let messages = [
+// // //    "hey,how is it going?",
+// // //    "I'm great, thank you! how about you?",
+// // //    "All good. been working on my portfolio today",
+// // //   "Same here",
+// // //   "Glad to hear that"
+// // // ]
+// // // for ( let u = 0; u < 5; u += 1  ){
+// // //    console.log(messages[u])
+// // // }
+// // // for ( let m = 10; m < 101; m += 10  ){
+// // //    console.log(m)
+// // // }
+// // // let cards = [7,4,9]
+// // // for(let u = 0; u < 10; u++){
+// // //    console.log(cards[u])
+// // // }
+
+// // // let player1 = 103;
+// // // let player2 = 109;
+
+// // // function total(){
+// // //    return player1 + player2 
+
+// // // }
+// // // let totalTime = total() 
+// // // console.log(totalTime)
+// // //  Math.floor (Math.random() * 6) + 1
+// // // let notRandom = Math.random()*5
+
+// // // let soRandom = Math.random()*7
+
+
+// // // console.log(getRAndom)
+// // // console.log(notRandom)
+// // // console.log(soRandom)
+
+// // // let flooredNumbers = Math.floor(3.6567)
+// // // let floordNumbers = Math.floor(7.7567)
+// // // let flooedNumbers = Math.floor(5.4567)
+// // // console.log(flooredNumbers)
+// // // console.log(floordNumbers)
+// // // console.log(flooedNumbers)
+
+// // // A function that returns a random number.
+// // // function rollDice(){
+// // //    let randomNo =  Math.floor (Math.random() * 6) + 1
+   
+// // // } 
+// // // console.log(rollDice) 
+// // // function rollDice(){
+ 
+// // //    let randomNumber =   Math.floor (Math.random() * 13) + 1
+// // //  if (randomNumber ===  1){
+// // //       return 11
+// // //      }else if (randomNumber > 10){
+// // //       return 10
+// // //      }  
+// // //       else {
+// // //      return randomNumber
+// // //    }
+// // // } console.log(rollDice()) 
+// // //  let likeDocumentaries= false;
+// // // let likeStartups = true;
+
+// // // if (likeDocumentaries === true || likeStartups === true){
+// // //  recommendMovies()
+// // // }
+// // // function recommendMovies(){
+// // // console.log(" Yo!! this ia a movie you can watch")
+// // // }
+// // // let course = {
+// // //   tags: ["html","css"]
+// // // }
+// // // console.log(course.tags[0,1])
+// // // let apartment = {
+// // //   modern: true,
+// // //   details: "loft style suite with wifi",
+// // //   beds: 2,
+// // //   arrival:["bed","bath","kitchen"],
+
+
+// // // }
+// // // console.log(apartment.arrival)
+// // // c 
+// // let person = {
+// //   name: "Munna",
+// //   age: 19,
+// //   country: "Nigeria"
+
+// // }
+// // function logData(){
+// //   console.log( person.name+" "+"is"+" "+ person.age +" "+"years old and comes from" +" "+ person.country)
+  
+// // }
+// //   logData() 
+
+// let age = 99;
+
+// if (age < 6){
+//  console.log('Free')
+// }
+// else if (age >= 6 && age < 17){
+//  console.log("child discount")
+// }
+// else if (age >= 18 && age <= 26){
+// console.log('student discount')  
+
+// }else if (age >= 27 && age <= 66){
+//  console.log('Full price') 
+// }else {
+  // console.log('senior citiozen discunt')
+
+// // }
+// console.log("The top 5 largest countries in the world")
+// let largeCountries = ['china','india','USA','Indonosia','Pakistan'] 
+// for (let i = 0; i < largeCountries.length; i++){
+//   console.log('-' + largeCountries[i])
+// } 
+// largeCountries.pop()
+// largeCountries.push("solkivia")
+// largeCountries.shift()
+// largeCountries.unshift("portugal")
+// largeCountries.push("Nigeria")
+
+
+// console.log(largeCountries)
+// 
+// function sumInput(){
+//  let numbers = [];
+//  let value = prompt("Give me a Number please,");
+
+//   if (value === "" || value === NaN || !isFinite(value)) break;
+   
+//   numbers.push(+value);
+
+//  }
+
+// let arr = [2,3,4,5,6,7,8,9]
+// function printArray(arr){
+//   for(let i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+//   }
+// }
+//   printArray(arr)
+    
+// let arr = [0,1,1,2,1,0,3,4,2,2];
+// printArray(arr);
+// //Once you have confirmed that this code snippet works
+// //add your lines of code here, by removing this comments
+
+// function printArray(my_array)
+// {
+//   for(let i = 0; i < my_array.length; i++)
+//   {
+//     console.log(my_array[i]);
+//   }
+// }
+
+let arr = [0,1,1,2,1,0,3,4,2,2];
+ let ar = [2,4,6,8,0,9,7,5,3,1];
+ let a = [1,3,5,7,9,0,8,6,4,2];
+ printArray(arr);
+//  printArray(ar);
+//  printArray(a);
+ //Once you have confirmed that this code snippet works
+//  for(var i = 0; i < 3; i++ ){
+    arr[arr.length - 1] = arr[6] 
+     
+    
+//  }
+printArray(arr);
+ 
+
+
+ 
+ function printArray(my_array)
+ {
+   for(let i = 0; i < my_array.length; i++)
+   {
+     console.log(my_array[i]);
+   }
+ }
